5753d64a1e229bc42244348c2708bc233af3d093
AliHaidarAzizi <135135657+AliHaidarAzizi@users.noreply.github.com>
2023-11-07 23:40:11 +0800

    adding more controllers

---
+++ src/controllers/visitor/listAll.js
@@ -0,0 +1,27 @@
+import Visitor from "../../model/visitor";
+import { parseMessage } from "../../utils/helper";
+
+const listAllVisitor = async (req, res) => {
+  try {
+    const { venueId } = req.params;
+    const visitors = await Visitor.findAll({
+      where: { venueId: venueId },
+    });
+    const list = visitors;
+    const length = list.length;
+    // console.log(DataTypes.list);
+    if (list === null) {
+      res.status(404).json(parseMessage("Link not found"));
+      return;
+    }
+
+    res.status(201).json(parseMessage(`${length} event(s) retrieved`, list));
+    return;
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error });
+    console.log(">>>>>>", error);
+    return;
+  }
+};
+
+export default listAllVisitor;
