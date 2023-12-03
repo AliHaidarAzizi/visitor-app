a1eb60945039937d54766157ce9cd60b20f83484
AliHaidarAzizi <135135657+AliHaidarAzizi@users.noreply.github.com>
2023-12-03 23:45:55 +0800

    improve viewAll controller for visitor to allow search filtering

--- src/controllers/visitor/listAll.js
+++ src/controllers/visitor/listAll.js
@@ -1,20 +1,37 @@
 import Visitor from "../../model/visitor";
 import { parseMessage } from "../../utils/helper";
+import { Op } from "sequelize";
 
 const listAllVisitor = async (req, res) => {
   try {
     const { venueId } = req.params;
+    const { search } = req.query;
     const page = req.query.page || 1;
     const limit = 15;
     const offset = (page - 1) * limit;
+    const whereCondition = { venueId };
+    if (search) {
+      whereCondition[Op.or] = [
+        { visitorName: { [Op.like]: `%${search}%` } },
+        { visitorEmail: { [Op.like]: `%${search}%` } },
+      ];
+    }
 
     const { count, rows: visitors } = await Visitor.findAndCountAll({
       limit: limit,
       offset: offset,
 
-      where: { venueId: venueId },
+      where: whereCondition,
+      // where: {
+      //   venueId: venueId,
+      //   [Op.or]: [
+      //     { name: { [Op.like]: `%${search}%` } },
+      //     { email: { [Op.like]: `%${search}%` } },
+      //   ],
+      // },
     });
     const list = visitors;
+    console.log(list);
     const length = list.length;
     const maxPage = Math.ceil(count / limit);
     // console.log(DataTypes.list);
